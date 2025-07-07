10a5c54330948c7f7ae0ebd8dff4f6e32c8fa1f5
Antiblue <110364372+Snapdev643@users.noreply.github.com>
2025-07-06 21:45:40 -0500

    Un-debug

--- external.py
+++ external.py
@@ -6,12 +6,12 @@ class Settings:
     def __init__(self):
         self.hz = 3.58e6 #3.58MHz
         self.fps = self.hz/59666.6666667 #roughly 60Hz
-        self.showFPS = True #Always show FPS in the top left
+        self.showFPS = False #Always show FPS in the top left
         
         self.perepherals = ["keyboard", "mouse"] #avaliable perepherals: keyboard, mouse, gamepad, microphone (only 2 at a time)
         
-        self.showSplash = False #show splash screen on boot
-        self.startupJingle = False #toggle the startup jingle on boot
+        self.showSplash = True #show splash screen on boot
+        self.startupJingle = True #toggle the startup jingle on boot
         
         
         self.corruptDisplay = False #mess with drawPixel to fuck with the screen (This affects nothing, and is entirely reversable)
